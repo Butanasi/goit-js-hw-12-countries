cba1da6717c74837e6494ce18a208275163d2afe
Danylo Nehreba <butanasiland@gmail.com>
2021-10-20 00:51:07 +0300

    handelbars

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,30 @@
-import countriCards from '../templates/countries-card.hbs'
+import countryCards from '../templates/countries-card.hbs'
+const ulEl = document.querySelector('.list')
+const inputEl = document.querySelector('.input')
+inputEl.addEventListener('input', onSearch)
 
-fetch('https://restcountries.com/v2/name/ukraine')
+function onSearch(e) {
+    e.preventDefault();
+    const name = e.currentTarget.value
+    console.log(name)
+    fetchCountryByName(name)
+        .then(renderCard)
+        .catch(error => console.log(error))
+        
+    
+}
+
+
+
+function fetchCountryByName(countryName) {
+    return fetch(`https://restcountries.com/v2/name/${countryName}`)
     .then(response => {
     return response.json()
     })
-    .then( count  => {
-        console.log(count)
-        const markup = countriCards(count);
+    
+}
+function renderCard(country) {
+     const markup = countryCards(country);
         console.log(markup)
-    })
-    .catch(error => {
-      console.log(error)  
-    })
+        ulEl.innerHTML=markup
+}
